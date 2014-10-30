8141d45c423a6ce91618b1f91cd4f948e07d1e73
Jens Lind <jens.lind@oddhill.se>
2014-10-30 19:34:28 +0100

    Added Gruntfile.js

---
+++ app/Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  grunt.registerTask('watch', [ 'watch' ]);
+
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourcemap: "none",
+      },
+      style: {
+        files: {
+          "css/main.css": "sass/main.scss",
+          "css/print.css": "sass/print.scss",
+          "css/normalize.css": "sass/normalize.scss"
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: ['js/**/*.js'],
+        tasks: ['jshint'],
+        options: {
+          livereload: false,
+        }
+      },
+      css: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass:style'],
+        options: {
+          livereload: false,
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+};
