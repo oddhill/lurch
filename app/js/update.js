3fb300897f77fe0ee46ef7f2dc941b68a2f796e2
Jens Lind <jens.lind@oddhill.se>
2014-11-20 19:33:37 +0100

    Run updateCheck only if manifestUrl is set in manifest

--- app/js/update.js
+++ app/js/update.js
@@ -1,12 +1,15 @@
 // Check for updates
 var update = require('./lib/update.js');
 
-/*update.checkNewVersion(function(newVersion, manifest) {
-  if (newVersion) {
-    $('body').prepend('<a href="#" class="install-new">Download & install update</a>');
-    $('.install-new').click(function(event) {
-      event.preventDefault();
-      //update.install(manifest);
-    });
-  }
-});*/
+var manifest = require('./package.json');
+if (manifest.manifestUrl) {
+  update.checkNewVersion(function(newVersion, manifest) {
+    if (newVersion) {
+      $('body').prepend('<a href="#" class="install-new">Download & install update</a>');
+      $('.install-new').click(function(event) {
+        event.preventDefault();
+        update.install(manifest);
+      });
+    }
+  });
+}
