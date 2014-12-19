a0520c4595e58cdae1a26395eb234e84c23a2cb2
Jens Lind <jens.lind@oddhill.se>
2014-12-19 19:11:39 +0100

    Save tutorial status
    Run tutorial only first startup.

--- app/js/tutorial.js
+++ app/js/tutorial.js
@@ -1,40 +1,50 @@
 $('document').ready(function() {
 
-  $('#tutorial').load('tutorial.html', function() {
+  if (localStorage.tutorial != 'false') {
 
-    $('#tutorial-slide').owlCarousel({
-      mouseDrag: false,
-      touchDrag: false
+    $('#tutorial').load('tutorial.html', function() {
+
+      $('#tutorial-slide').owlCarousel({
+        mouseDrag: false,
+        touchDrag: false
+      });
+
+      var slider = $("#tutorial-slide").data('owlCarousel');
+
+      // Move to next
+      $('.step .next').click(function() {
+        if ($(this).hasClass('last')) {
+          $('.lurch-success').addClass('on');
+        }
+        slider.next();
+      });
+
+      // Skip tutorial
+      $('.step .skip').click(function() {
+        localStorage.tutorial = 'false';
+        $('#app').load('views/manage_projects.html');
+        $('#tutorial').remove();
+      });
+
+      // Finish (close tutorial)
+      $('.step .finish').click(function() {
+        localStorage.tutorial = 'false';
+        $('#app').load('views/manage_projects.html');
+        $('#tutorial').remove();
+      });
+
+      // Drag and drop
+      var projectHolder = document.getElementById('tutorial-drag-drop-project');
+      var pluginHolder = document.getElementById('tutorial-drag-drop-plugin');
+      dragndrop(projectHolder);
+      dragndrop(pluginHolder);
+
     });
 
-    var slider = $("#tutorial-slide").data('owlCarousel');
-
-    // Move to next
-    $('.step .next').click(function() {
-      if ($(this).hasClass('last')) {
-        $('.lurch-success').addClass('on');
-      }
-      slider.next();
-    });
-
-    // Skip tutorial
-    $('.step .skip').click(function() {
-      $('#app').load('views/manage_projects.html');
-      $('#tutorial').remove();
-    });
-
-    // Finish (close tutorial)
-    $('.step .finish').click(function() {
-      $('#app').load('views/manage_projects.html');
-      $('#tutorial').remove();
-    });
-
-    // Drag and drop
-    var projectHolder = document.getElementById('tutorial-drag-drop-project');
-    var pluginHolder = document.getElementById('tutorial-drag-drop-plugin');
-    dragndrop(projectHolder);
-    dragndrop(pluginHolder);
-
-  });
+  } else {
+    // Load the app
+    $('#app').load('views/manage_projects.html');
+    $('#tutorial').remove();
+  }
 
 });
