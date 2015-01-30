ac33a4e8061d97cb9ad565c722885452966e4932
Jens Lind <jens.lind@oddhill.se>
2015-01-30 13:40:32 +0100

    Count projects and plugins

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,19 @@
+var Project = require('./lib/Project/Project.js');
+var Plugin = require('./lib/Plugin/Plugin.js');
+
 $(document).ready(function() {
+
+  Project.findAll(function(err, res) {
+    if (!err) {
+      $('.main-nav .projects-calc').html(res.length);
+    }
+  });
+
+  Plugin.findAll(function(err, res) {
+    if (!err) {
+      $('.main-nav .plugins-calc').html(res.length);
+    }
+  });
 
   $('.main-nav .projects').click(function() {
     $('#app').load('views/manage_projects.html');
