553fb9df76f1b9535dbef3b844babd3c80744daf
Jens Lind <jens.lind@oddhill.se>
2015-02-05 21:39:06 +0100

    Save db fields on new project

--- app/controllers/add_project.js
+++ app/controllers/add_project.js
@@ -1,15 +1,35 @@
 var Project = require('./lib/Project/Project.js');
 
+// File dialog when click
+$('#dragdrop-project').on('click', function() {
+  $('input[name=path]').click();
+});
+
 $('#add-site').submit(function(event) {
   event.preventDefault();
 
+  $('body').prepend('<div class="msg loading"><span class="icon"></span><span class="message">Success!</span></div>');
+
   var name = $(this).find('input[name="name"]').val();
   var path = $(this).find('input[name="path"]').val();
+  var remotePath = $(this).find('input[name="remote"]').val();
+  var db = {
+    host: $(this).find('input[name="db-host"]').val(),
+    port: $(this).find('input[name="db-port"]').val(),
+    name: $(this).find('input[name="db-name"]').val(),
+    username: $(this).find('input[name="db-username"]').val(),
+    password: $(this).find('input[name="db-password"]').val()
+  };
 
-  var newProject = new Project(name, path, [], false, null);
+  var newProject = new Project(name, path, [], false, null, db);
   newProject.save(function(err) {
-    // Rebuild menu
-    nwMenu.rebuild();
-    $('#app').load('views/manage_projects.html');
+    if (!err) {
+      // Rebuild menu
+      nwMenu.rebuild();
+
+      $('.msg').removeClass('loading').addClass('success');
+    } else {
+      $('.msg').removeClass('loading').addClass('fail');
+    }
   });
 });
