3e20389ec27e02577549592a87a3e594d21ec148
Jens Lind <jens.lind@oddhill.se>
2015-02-23 20:42:00 +0100

    Settings to angular WIP

--- app/js/index.js
+++ app/js/index.js
@@ -1,31 +1,6 @@
 (function() {
 
-  //require('nw.gui').Window.get().showDevTools().resizeTo(800, 1000);
-
   $(document).ready(function() {
-
-    // Settings click event
-    $('.footer .settings').on('click', function(e) {
-      e.preventDefault();
-
-      var close = false;
-
-      // Show settings
-      $('.main-content.main').addClass('minimize');
-
-      // Close handler
-      $('.main-content.main.minimize').on('click', function(e) {
-        e.preventDefault();
-        if (close) {
-          $(this).removeClass('minimize');
-          $(this).off(e);
-        }
-        close = true;
-      });
-    });
-
-    // Load rest token to settings page
-    $('.main-content.settings .key').html(localStorage.restToken);
 
     // Settings saving handler
     Settings.save();
--- app/ng-controllers/IndexCtrl.js
+++ app/ng-controllers/IndexCtrl.js
@@ -1,5 +1,9 @@
-lurchApp.controller('IndexCtrl', ['$scope', function ($scope) {
+lurchApp.controller('IndexCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
   $scope.pluginForm = false;
+  $scope.allowSettingsClose = false;
+
+  // REST-API key
+  $scope.restToken = localStorage.restToken;
 
   // Hide plugin form
   $scope.showPluginForm = function () {
@@ -9,5 +13,22 @@ lurchApp.controller('IndexCtrl', ['$scope', function ($scope) {
   // Show plugin form
   $scope.hidePluginForm = function () {
     $scope.pluginForm = false;
+  };
+
+  // Show settings
+  $scope.showSettings = function () {
+    $scope.minimizeMain = 'minimize';
+
+    $timeout(function() {
+      $scope.allowSettingsClose = true;
+    }, 200);
+  };
+
+  // Hide settings
+  $scope.hideSettings = function () {
+    if ($scope.allowSettingsClose) {
+      $scope.minimizeMain = '';
+      $scope.allowSettingsClose = false;
+    }
   };
 }]);
