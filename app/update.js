749908c2fcd89072f0cbb80f9dd04fdebc742f56
Jens Lind <jens.lind@oddhill.se>
2015-03-08 12:37:05 +0100

    Update markup

--- app/update.js
+++ app/update.js
@@ -5,10 +5,18 @@ var manifest = require('./package.json');
 if (manifest.manifestUrl) {
   update.checkNewVersion(function(newVersion, manifest) {
     if (newVersion) {
-      $('body').prepend('<a href="#" class="install-new">Download & install update</a>');
-      $('.install-new').click(function(event) {
+      $('body .frame').prepend('<div class="new-version"><h3>New version available</h3><p class="version">' + manifest.version + '</p><a href="#" class="download">Install update</a><a class="skip" href="#">Skip update</a></div>');
+      $('.download').click(function(event) {
         event.preventDefault();
         update.install(manifest);
+        $(this).html('<div class="loader"></div>');
+        $(this).addClass('downloading');
+        $('.new-version .skip').remove();
+      });
+
+      $('.skip').click(function(event) {
+      	event.preventDefault();
+      	$('.new-version').remove();
       });
     }
   });
