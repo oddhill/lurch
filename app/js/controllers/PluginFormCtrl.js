6a7a69451e86af60f9a4259ce6361ec5041ed007
Jens Lind <jens.lind@oddhill.se>
2015-03-09 21:47:32 +0100

    Added browse support on add plugin

--- app/js/controllers/PluginFormCtrl.js
+++ app/js/controllers/PluginFormCtrl.js
@@ -1,6 +1,9 @@
 lurchApp.controller('PluginFormCtrl', ['$scope', '$rootScope', '$location', '$timeout', 'Plugin', function ($scope, $rootScope, $location, $timeout, Plugin) {
   // Add a plugin
   $scope.add = function () {
+    if (!$scope.newPluginPath) {
+      $scope.newPluginPath = document.getElementById('plugin-path').value;
+    }
     Plugin.add($scope.newPluginPath).then(function(data) {
       if (data.success) {
         // Rebuild menu
@@ -15,6 +18,15 @@ lurchApp.controller('PluginFormCtrl', ['$scope', '$rootScope', '$location', '$ti
           $scope.$apply();
         });
       }
+    });
+  };
+
+  // Browse project
+  $scope.browse = function () {
+    var browsePath = document.getElementById('plugin-path');
+    $timeout(function() {
+      browsePath.click();
+      global.nwWindow.focus();
     });
   };
 }]);
