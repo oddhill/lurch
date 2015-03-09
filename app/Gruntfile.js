30e0164370c14ad1bf0e80e86a27d5c230634303
Jens Lind <jens.lind@oddhill.se>
2015-03-09 21:45:38 +0100

    Moved build cache dir

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -7,6 +7,7 @@ module.exports = function(grunt) {
       options: {
         platforms: ['osx'],
         buildDir: '../builds',
+        cacheDir: '../cache'
       },
       src: ['./**/*'],
     },
